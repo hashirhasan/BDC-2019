b4e38e8f764edd096d0da2f7ca4da468c2d36474
Hashir13 <hasanhashir1314@gmail.com>
2019-04-16 22:47:59 +0530

    final code

--- admin/connect.php
+++ admin/connect.php
@@ -1,6 +1,8 @@
 <?php
-   
-$connection=mysqli_connect('localhost','root','','bdc');                   //making connection to the database medicare1
+   require('../credentials.php');
+    $host = host;
+	$dbname = dbname;
+$connection=mysqli_connect($host,username,password,$dbname);                   //making connection to the database medicare1
     if(!$connection){
         die("connection failed");
     }
